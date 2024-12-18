53a4c38e2e3f10e89080b49da517f93d6fbd876d
mario-minesec <mario.gamal@minesecsoftpos.com>
2024-12-18 13:00:01 +0200

    Initial Commit

---
+++ TapToAuth Demo/TapToAuth_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TapToAuth_DemoApp.swift
+//  TapToAuth Demo
+//
+//  Created by Admin on 18/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct TapToAuth_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
