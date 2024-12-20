f4d5b6ffa0d5617148b0a42fc4ce0e54fb06ee82
mario-minesec <mario.gamal@minesecsoftpos.com>
2024-12-20 22:50:56 +0200

    Add FCM messaging

--- TapToAuth Demo/TapToAuth_DemoApp.swift
+++ TapToAuth Demo/TapToAuth_DemoApp.swift
@@ -6,9 +6,55 @@
 //
 
 import SwiftUI
+import FirebaseCore
+import FirebaseMessaging
+import UserNotifications
+
+class AppDelegate: NSObject, UIApplicationDelegate, UNUserNotificationCenterDelegate, MessagingDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    UNUserNotificationCenter.current().delegate = self
+    UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { granted, error in
+        if let error = error {
+            print("Notification permission request failed: \(error.localizedDescription)")
+        } else {
+            print("Notification permission granted: \(granted)")
+        }
+    }
+    application.registerForRemoteNotifications()
+    Messaging.messaging().delegate = self
+    return true
+  }
+    
+  func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+      Messaging.messaging().apnsToken = deviceToken
+  }
+  
+  func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+      print("FCM Token: \(fcmToken ?? "")")
+  }
+  
+  func userNotificationCenter(_ center: UNUserNotificationCenter,
+                              willPresent notification: UNNotification,
+                              withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+      completionHandler([.banner, .badge, .sound])
+  }
+
+  func userNotificationCenter(_ center: UNUserNotificationCenter,
+                              didReceive response: UNNotificationResponse,
+                              withCompletionHandler completionHandler: @escaping () -> Void) {
+      let userInfo = response.notification.request.content.userInfo
+      print("User tapped notification: \(userInfo)")
+      completionHandler()
+  }
+}
 
 @main
 struct TapToAuth_DemoApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
